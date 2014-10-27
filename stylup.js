386d2ef49374e5fa22d937dfcf0d2a14d858c9f9
Gavin McFarland <sevenupcan@gmail.com>
2014-10-27 13:52:51 +0000

    First working version of script

--- stylup.js
+++ stylup.js
@@ -1,78 +1,180 @@
+var token = {
+    query: /@(\w+)\((.+?)\)/gim,
+    property: /\[([\w-]+)\s([\w\s/!:@-]+)\]/gim,
+    shortQuery: /([\w-:/]+)@([\w!]+)/gim,
+    normal: /[\w-/]+/gmi
+};
 
-var string = "[something] @small(something) Something @something([something something])";
+var makeRegex = function() {
+
+  var array = [];
+
+  Object.keys(token).forEach(function(key) {
+    array.push(token[key].source);
+  });
+
+  var regex = array.join("|");
+
+  return new RegExp(regex, "gmi");
+
+};
+
+var split = function(input) {
+  return input.match(makeRegex());
+};
+
+var parse = function(input) {
+  var array, htmlClass, item, output, output, _i, _j, _a, _len, _len1, len2;
+
+  array = split(input);
+
+  output = [];
+
+  for (_i = 0, _len = array.length; _i < _len; _i++) {
+    item = array[_i];
+
+    input = item;
+
+    // 1.0
+    if (input.match(token.query)) {
+
+      input.replace(token.query, function(match, p1, p2) {
+
+        var htmlClass, query;
+        query = p1;
+        input = p2;
+
+        input = split(input);
+
+        for(var _g = 0; _g < input.length; _g++) {
+
+         // 1.1
+         if (input[_g].match(token.property)) {
+
+          input[_g].replace(token.property, function(match, p1, p2) {
+            var htmlClass, values;
+            htmlClass = p1;
+            values = p2;
+
+            var values = split(values);
 
 
-function parse(string) {
+            for(var _z = 0; _z < values.length; _z++) {
 
-  // This joins several regexes and turns them into one regex
+              output.push({
+                htmlClass: htmlClass,
+                value: values[_z],
+                query: query
+              });
+            }
+          });
+        }
 
-  function joinRe() {
-    var tokens = {
-      context  : /@.+?\(.+?\)/,
-      method   : /\[.+?\]/,
-      object   : /[\w]+/
-    };
+        // 1.2
+        else {
+          output.push({
+            htmlClass: input[_g],
+            query: query
+          });
+        }
 
-    var array = [];
+        }
 
-    Object.keys(tokens).forEach(function(key) {
-      array.push(tokens[key].source);
-    });
+      });
+    }
 
-    return "(" + array.join("|") + ")";
+    // 2.0
+    else if (input.match(token.property)) {
 
-  }
+      input.replace(token.property, function(match, p1, p2) {
+        var htmlClass, values;
+        htmlClass = p1;
+        input = p2;
+        input = split(input);
 
-  // This creates an array using the full regex
 
-  function makeArray(string) {
-    var pattern = new RegExp(joinRe(), "g");
+        for(var _d = 0; _d < input.length; _d++) {
 
-    var array = [];
+          // 2.1
+          if (input[_d].match(token.shortQuery)) {
+            input[_d].replace(token.shortQuery, function(match, p1, p2) {
+              var query, values;
+              values = p1;
+              query = p2;
+              output.push({
+                htmlClass: htmlClass,
+                value: values,
+                query: query
+              });
+            });
+          }
 
-    string.replace(pattern, function(match, p1, p2, p3){
-      if(p1) array.push(p1);
-    });
+          // 2.2
+          else {
+            output.push({
+              htmlClass: htmlClass,
+              value: input[_d]
+            });
+          }
+        }
+      });
+    }
 
-    return array;
-  }
+    // 3.0
+    else if (input.match(token.shortQuery)) {
+      input.replace(token.shortQuery, function(match, p1, p2) {
+        var htmlClass, query;
+        htmlClass = p1;
+        query = p2;
+        return output.push({
+          htmlClass: htmlClass,
+          query: query
+        });
+      });
+    }
 
-  // Test: Trying to check each string in array for whether it is a context, method or object.
-
-  var newArray = makeArray(string);
-
-  for(var i = 0; i < newArray.length; i++) {
-    if(newArray[i] == "@small(something)") {
-      // Just a test to replace string in array
-      newArray[i] = newArray[i].replace(/@(.+?)\((.+?)\)/g, "$2-$1");
-      console.log(newArray[i]);
+    // 4.0
+    else {
+      htmlClass = input;
+      output.push({
+        htmlClass: htmlClass
+      });
     }
   }
 
-  return newArray;
+  return render(output);
+};
 
 
+var render = function(input) {
+  var output = [];
 
-}
+  for (var i = 0; i < input.length; i++) {
+    var object = input[i];
 
+    var array = [];
+    for (var property in object) {
+      if (object.hasOwnProperty(property)){
+        array.push(object[property]);
+      }
+    }
 
-console.log(parse(string));
+    array = array.join("-");
+    output.push(array);
+  }
 
+  return output.join(' ');
 
+};
 
+var stylup = function() {
+  elementList = document.querySelectorAll("[class]");
 
+  for (_i = 0, _len = elementList.length; _i < _len; _i++) {
+    item = elementList[_i];
 
+    item.className = parse(item.className);
+  }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+stylup();
